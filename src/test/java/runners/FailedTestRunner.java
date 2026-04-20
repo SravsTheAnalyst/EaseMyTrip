67f8fd06038eb0796559d9b20665cc48b42d705c
Sravanti <Sravs@gmail.com>
2026-04-20 09:20:34 +0530

    Ease my trip

---
+++ src/test/java/runners/FailedTestRunner.java
@@ -0,0 +1,14 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+	features = "@target/failed_scenarios.txt",
+    glue = {"stepDefinitions","hooks"},
+    plugin = {"pretty" , "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
+    monochrome = true
+    )
+public class FailedTestRunner extends AbstractTestNGCucumberTests{
+
+}
